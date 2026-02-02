fb0ab5cb5a9755ac2a39c49fdfa4a9d9bed7b14c
Siddhi Ramulu Manche <158692933+Siddhidesign@users.noreply.github.com>
2026-02-02 09:55:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const extraProjects = [
         title: "Real-Time Identification of Medicinal Plants",
         category: "IoT Design",
         img: "https://picsum.photos/600/400?random=14", // Replace with your actual image path
-        url: "medicine.html",
+        url: "https://github.com/Siddhidesign/Real-time-Identification-of-Medicinal-Plants?tab=readme-ov-file",
         overlay: "View Case Study"
     }
 ];
